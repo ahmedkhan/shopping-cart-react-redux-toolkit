7e265fef3dff5211830f761ff83af125aaf07e25
Ahmed Khan <ahmed.khan8845@gmail.com>
2020-11-06 05:40:42 +0500

    finial

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import {Basket} from '../src/components/Basket'; 
 import { Provider } from "react-redux";
 import { store } from "./store";
-import './App.css';
+import { Container, Box } from "@material-ui/core";
 import { Product } from './components/Product';
 
 function App() {
   return (
-   <Provider store={store}> 
-    
-     <Basket/> 
-     <Product/>     
-     
-   </Provider> 
+   <Provider store={store}>
+    <Container maxWidth="md">     
+      <Box mt={5} mb={5}>
+        <Product />
+      </Box>
+      <Box mt={5} mb={5}>
+        <Basket />
+      </Box>
+    </Container>
+   </Provider>  
   );
 }
 
